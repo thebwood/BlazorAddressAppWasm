8a858dfc4bc5846b89ef9124d96004c243a5d17c
Brian David Wood <brian_d_wood@yahoo.com>
2024-01-21 19:57:06 -0500

    Adding common base methods

--- BlazorAddressAppWasm.Web/BaseClasses/CommonBase.cs
+++ BlazorAddressAppWasm.Web/BaseClasses/CommonBase.cs
@@ -1,8 +1,36 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace BlazorAddressAppWasm.Web.BaseClasses
 {
     public class CommonBase : ComponentBase
     {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public JSRuntime JScript { get; set; }
+
+        public async void ConsoleLog(string message)
+        {
+            await JScript.InvokeVoidAsync("console.log", message);
+        }
+
+
+        public async void ConsoleError(string message)
+        {
+            await JScript.InvokeVoidAsync("console.error", message);
+        }
+
+        public async void ConsoleWarn(string message)
+        {
+            await JScript.InvokeVoidAsync("console.warn", message);
+        }
+
+        public async void ConsoleInfo(string message)
+        {
+            await JScript.InvokeVoidAsync("console.info", message);
+        }
+
     }
 }
